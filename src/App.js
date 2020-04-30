a7dd3623dc9671c208640655137fd9f9b20f5867
Zeniith <sebastian.binkowski.97@gmail.com>
2020-04-30 15:51:12 +0200

    Add basic nav component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { Navigation } from "components";
+import theme from "utils/theme";
+import GlobalStyles from "./index.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Router>
+        <Navigation
+          items={[
+            {
+              content: "Homepage",
+              to: "/",
+            },
+            {
+              content: "Budget page",
+              to: "/budget",
+            },
+          ]}
+        />
+        <Switch>
+          <Route exact path="/">
+            Homepage
+          </Route>
+          <Route path="/budget">BudgetPage</Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
